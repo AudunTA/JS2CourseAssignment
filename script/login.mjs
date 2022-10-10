91e051a8f50a17230f226c5d2b947cf8b9a64ba4
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-10-10 20:39:46 +0200

    worked on register

--- script/login.mjs
+++ script/login.mjs
@@ -42,12 +42,45 @@ loginForm.addEventListener("submit", (e) => {
   login("/social/auth/login");
 });
 
-//------------------------------- create user -----------------------------
+//------------------------------- Create user -----------------------------
 
-const headline = documen.querySelector(".headline");
+const headline = document.querySelector(".headline");
 const createAccount = document.querySelector(".create_account");
+const createAccForm = document.querySelector(".create-acc-form");
 
-createAccount.addEventListener("click" (e) =>{
-    e.preventDefault();
-    
-})
+createAccount.addEventListener("click", (e) => {
+  e.preventDefault();
+  headline.innerHTML = "<h1>Create Account</h1>";
+  createAccount.classList.remove("username");
+});
+
+createAccForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.querySelector(".input_email").value;
+  const password = document.querySelector(".input_password").value;
+  const userName = document.querySelector(".input_username").value;
+  const user = {
+    name: userName,
+    email: email,
+    password: password,
+  };
+  console.log(email, password, userName);
+
+  async function register(endpoint) {
+    try {
+      const request = await fetch(`${baseUrl}${endpoint}`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(user),
+      });
+      const results = await request.json();
+      console.log(results);
+      console.log(request.status);
+    } catch (e) {
+      console.log("this is the error:" + e);
+    }
+  }
+  register("/social/auth/register");
+});
